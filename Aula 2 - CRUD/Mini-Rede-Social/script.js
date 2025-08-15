53141054f7c313a66145eff0379553b1dc7cd06a
Miguel <miguelmanfre0603@gmail.com>
2025-08-14 22:30:19 -0300

    CRUD

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -15,6 +15,70 @@ let posts = [
     {
         text: "Este é o terceiro post teste",
         category: "Eventos",
+        image: "https://placedog.net/150?random=3",
         date: "12/10/2023 12:00:00"
     }
 ];
+
+window.onload = function() {
+    displayPosts();
+    document.querySelector("#postForm").addEventListener("submit", addPost);
+};
+
+// CRUD - Create, Read, Update, Delete
+// Create
+function addPost(e) {
+    e.preventDefault(); // Evita o reset do formulário
+    const text = document.querySelector("#postText").value;
+    const category = document.querySelector("#postCategory").value;
+    const image = document.querySelector("#postImage").value;
+
+    const newPost = {
+        text: text,
+        category: category,
+        image: image,
+        date: new Date().toLocaleString("pt-BR")
+    }
+
+    posts.unshift(newPost); // Adiciona o novo post no início do array
+    displayPosts(); // Atualiza a exibição dos posts
+}
+// Read
+function displayPosts() {
+    const listaPosts = document.querySelector("#postList");// Seleciona o elemento onde os posts serão exibidos
+    listaPosts.innerHTML = ""; // Limpa a lista antes de exibir os posts
+    posts.forEach((pegaItem) =>{
+        const cardPost = document.createElement("div");
+        cardPost.classList.add("cardPost");
+        cardPost.innerHTML = `
+            <h2>${pegaItem.text}</h2>
+            <img src="${pegaItem.image}"/>
+            <p><strong>Categoria:</strong> ${pegaItem.category}</p>
+            <p><strong>Data e Hora:</strong> ${pegaItem.date}</p>
+            <div>
+                <button><img id="symbols" src="./editSymbol.png"/> Editar</button>
+                <button><img id="symbols" src="./deleteSymbol.png"/> Deletar</button>
+            </div>`;// Cria o HTML do post
+        listaPosts.append(cardPost);// Adiciona o post à lista
+    })
+}
+// Update
+function editPost() {}
+// Delete
+function deletePost() {}
+
+
+// Exemplo de objeto
+// const pessoa = {
+//     nome: "Miguel",
+//     idade: 18,
+//     tags: ["programador", "gamer", "estudante"],
+//     isAdmin: true,
+// }
+
+// console.log(pessoa.nome);
+// console.log(pessoa.idade);
+// console.log(pessoa.tags[0]);
+// console.log(pessoa.isAdmin);
+// console.log(pessoa["idade"]);
+
